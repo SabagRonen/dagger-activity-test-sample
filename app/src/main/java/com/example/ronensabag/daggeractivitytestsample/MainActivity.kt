5a039d2c6e04183ae620c46f9f6adfcc290be823
ronensabag <ronen@monday.com>
2019-08-30 22:19:13 +0300

    upgrade to dagger 2.24

--- app/src/main/java/com/example/ronensabag/daggeractivitytestsample/MainActivity.kt
+++ app/src/main/java/com/example/ronensabag/daggeractivitytestsample/MainActivity.kt
@@ -3,20 +3,18 @@ package com.example.ronensabag.daggeractivitytestsample
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
-import androidx.fragment.app.Fragment
 import dagger.android.AndroidInjection
 import dagger.android.DispatchingAndroidInjector
-import dagger.android.support.HasSupportFragmentInjector
+import dagger.android.HasAndroidInjector
 import kotlinx.android.synthetic.main.activity_main.toolbar
 
 import javax.inject.Inject
 
 
-class MainActivity : AppCompatActivity(), HasSupportFragmentInjector {
+class MainActivity : AppCompatActivity(), HasAndroidInjector {
+  @Inject lateinit var fragmentInjector: DispatchingAndroidInjector<Any>
 
-  @Inject lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
-
-  override fun supportFragmentInjector() = fragmentInjector
+  override fun androidInjector() = fragmentInjector
 
   override fun onCreate(savedInstanceState: Bundle?) {
     AndroidInjection.inject(this)
